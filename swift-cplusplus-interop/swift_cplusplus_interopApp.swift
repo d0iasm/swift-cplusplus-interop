2b9018dab01bd0c48243a6811071f33fa0ed55df
Asami Doi <asamidoi@chromium.org>
2025-09-16 15:28:54 +0200

    Initial Commit

---
+++ swift-cplusplus-interop/swift_cplusplus_interopApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_cplusplus_interopApp.swift
+//  swift-cplusplus-interop
+//
+//  Created by Asami Doi on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_cplusplus_interopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
